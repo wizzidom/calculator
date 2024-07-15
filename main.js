3b14670247b6a0988e488a5303ddf95bb3f3bc1e
wizzidom <majolawisdom@gmail.com>
2024-07-15 03:09:27 +0200

    Added the UI for the calculator

---
+++ main.js
@@ -0,0 +1,46 @@
+const screen = document.getElementById('screen');
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+const zero = document.getElementById('0');
+
+
+one = 1;
+two = 2;
+three = 3;
+four = 4;
+five = 5;
+six = 6;
+seven = 7;
+eight = 8;
+nine=9;
+zero = 0;
+
+
+function add(){
+
+}
+function multiply(){
+
+}
+function minus(){
+
+}
+function divide(){
+
+}
+function clear(){
+
+}
+function deletee(){
+
+}
+function equals(){
+    
+}
